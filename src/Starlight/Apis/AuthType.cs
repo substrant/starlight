2fa84d75759243d04f5b21b1b9859e5385ee3914
RealNickk <nick@substrant.dev>
2022-11-12 15:06:31 -0600

    Refactor AuthType.cs

--- src/Starlight/Apis/AuthType.cs
+++ src/Starlight/Apis/AuthType.cs
@@ -1,7 +1,17 @@
 ﻿namespace Starlight.Apis;
 
+/// <summary>
+///     The authentication method to use.
+/// </summary>
 public enum AuthType
 {
+    /// <summary>
+    ///     Authenticate using an authentication token (<c>.ROBLOSECURITY</c> cookie).
+    /// </summary>
     Token,
+
+    /// <summary>
+    ///     Authenticate using a one-time authentication ticket.
+    /// </summary>
     Ticket
 }
