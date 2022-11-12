9dce6f80a39abe50828e83a617ef21fc06453f82
RealNickk <nick@substrant.dev>
2022-11-12 15:32:14 -0600

    Refactor JoinType.cs

--- src/Starlight/Apis/JoinGame/JoinType.cs
+++ src/Starlight/Apis/JoinGame/JoinType.cs
@@ -1,8 +1,22 @@
 ﻿namespace Starlight.Apis.JoinGame;
 
+/// <summary>
+///     The method of joining to use.
+/// </summary>
 public enum JoinType
 {
+    /// <summary>
+    ///     Join a server that Roblox has selected for you.
+    /// </summary>
     Auto,
+
+    /// <summary>
+    ///     Join a specific server.
+    /// </summary>
     Specific,
+
+    /// <summary>
+    ///     Join a private or reserved server.
+    /// </summary>
     Private
 }
