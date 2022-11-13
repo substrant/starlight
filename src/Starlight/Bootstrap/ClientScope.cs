152188f62f2914a42521cfb09784f69ac47053c8
RealNickk <nick@substrant.dev>
2022-11-12 20:50:27 -0600

    Revert "Refactor ClientScope.cs"
    This reverts commit b56f7b793afbf3b703c4b467e1121c6130e0fa01.

--- src/Starlight/Bootstrap/ClientScope.cs
+++ src/Starlight/Bootstrap/ClientScope.cs
@@ -2,13 +2,6 @@
 
 public enum ClientScope
 {
-    /// <summary>
-    ///     Installations will be in the %localappdata%\Roblox\Versions folder.
-    /// </summary>
-    Global,
-
-    /// <summary>
-    ///     Installations will be in a folder called "Roblox" inside the current working directory.
-    /// </summary>
-    Local
+    Global, // %localappdata%/Roblox/Versions
+    Local // assemblyPath/Roblox
 }
