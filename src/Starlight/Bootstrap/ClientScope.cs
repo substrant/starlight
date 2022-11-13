e96846f49f48563a11b97807cc6f8dbf2c02d37a
RealNickk <nick@substrant.dev>
2022-11-12 20:51:50 -0600

    Refactor ClientScope.cs

--- src/Starlight/Bootstrap/ClientScope.cs
+++ src/Starlight/Bootstrap/ClientScope.cs
@@ -1,7 +1,17 @@
 ﻿namespace Starlight.Bootstrap;
 
+/// <summary>
+///     The scope (or installation parent directory) of a client.
+/// </summary>
 public enum ClientScope
 {
-    Global, // %localappdata%/Roblox/Versions
-    Local // assemblyPath/Roblox
+    /// <summary>
+    ///     Installations will be in the %localappdata%\Roblox\Versions folder.
+    /// </summary>
+    Global,
+
+    /// <summary>
+    ///     Installations will be in a folder called "Roblox" inside the current working directory.
+    /// </summary>
+    Local
 }
